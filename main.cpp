04e99bc2fb95c652644a94f2525b94b45de0edc0
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2024-10-30 16:50:52 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int nneg, i;
+    double max, min, media, np, n, sommapositivi;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    i = 0;
+    sommapositivi = 0;
+    np = 0;
+    nneg = 0;
+    do{
+        cout<<"inserisci il numero"<<endl;
+        cin>>n;
+        if(i==0){
+            max=n;
+            }else{
+                if(n>max){
+                    max=n;
+                    }
+
+                 }
+                 if(i==0){
+                    min=n;
+                    }else{
+                        if(n<min){
+                            min=n;
+                }
+            }
+            
+            
+                if(n>=0){
+                    sommapositivi=n+sommapositivi;
+                    np=np+1;
+                    }else{
+                        nneg=nneg+1;
+                        }
+                        i=i+1;
+                        }while(n!=0);
+                        cout<<"in numero massimo è"<<max<<endl;
+                        cout<<"il minimo è"<<min<<endl;
+                        cout<<"i valori negativi sono"<<nneg<<endl;
+                        cout<<"la media dei valori positivi è"<<sommapositivi/np<<endl;
+                        }
+
+
+
+    
